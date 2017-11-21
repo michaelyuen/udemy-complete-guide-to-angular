8f065ab9e86befcbe80dd633112fa75d00c59814
Michael Yuen <michael.yuen@ey.com>
2017-11-20 19:33:43 -0500

    Assignment 1

---
+++ src/app/warning-alert/warning-alert.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WarningAlertComponent } from './warning-alert.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WarningAlertComponent]
+})
+export class WarningAlertModule { }
